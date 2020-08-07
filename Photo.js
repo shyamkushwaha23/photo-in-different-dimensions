c36d68c4161c33b642e61d94ab23cfafb2851ed4
shyamkushwaha23 <shyamji1983@gmail.com>
2020-08-07 19:40:51 +0530

    added animation and reset button

--- Photo.js
+++ Photo.js
@@ -4,12 +4,17 @@ export default () => {
   let [size, setSize] = useState('');
   let [shape, setShape] = useState('');
 
-  function handleSize(event) {
+  const handleSize = (event) => {
     setSize(event.target.value);
   }
 
-  function handleShape(event) {
+  const handleShape = (event) => {
     setShape(event.target.value);
+  }
+
+  const reset = () => {
+    setSize('');
+    setShape('');
   }
 
   let figureClasses = `size-${size} shape-${shape}`;
@@ -19,29 +24,29 @@ export default () => {
       <h1 style={{textAlign: 'center'}}>Photo Functionality: </h1>
 
       <fieldset>
-        <legend>Select Photo Size:</legend>
+        <legend>Select Size</legend>
 
-        <label><input type="radio" name="size" value="small" onClick={handleSize} /> Small </label>
+        <label><input type="radio" name="size" value="small" checked={size === 'small'} onClick={handleSize} /> Small </label>
         <br />
 
-        <label><input type="radio" name="size" value="medium" ___checked={size === 'medium'} onClick={handleSize} /> Medium </label>
+        <label><input type="radio" name="size" value="medium" checked={size === 'medium'} onClick={handleSize} /> Medium </label>
         <br />
 
-        <label><input type="radio" name="size" value="large" onClick={handleSize} /> Large </label>
+        <label><input type="radio" name="size" value="large" checked={size === 'large'} onClick={handleSize} /> Large </label>
       </fieldset>
       <br />
 
 
       <fieldset>
-        <legend>Select Photo Shape:</legend>
+        <legend>Select Shape</legend>
 
-        <label><input type="radio" name="shape" value="rectangle" onClick={handleShape} /> Ractangle </label>
+        <label><input type="radio" name="shape" value="rectangle" checked={shape === 'rectangle'} onClick={handleShape} /> Ractangle </label>
         <br />
 
-        <label><input type="radio" name="shape" value="square" ___checked={shape === 'square'} onClick={handleShape} /> Square </label>
+        <label><input type="radio" name="shape" value="square" checked={shape === 'square'} onClick={handleShape} /> Square </label>
         <br />
 
-        <label><input type="radio" name="shape" value="circle" onClick={handleShape} /> Circle </label>
+        <label><input type="radio" name="shape" value="circle" checked={shape === 'circle'} onClick={handleShape} /> Circle </label>
       </fieldset>
       <br />
 
@@ -50,7 +55,12 @@ export default () => {
         <img src="http://akns-images.eonline.com/eol_images/Entire_Site/2015516/rs_300x300-150616073901-600.Donald-Trump.jl.061615.jpg" />
       </figure>
 
-      <p style={{textAlign: 'center'}}>Actual image is 300px x 300px</p>
+      <div style={{textAlign: 'center', color: '#666', fontSize: '.9rem'}}>Actual image is 300px x 300px</div>
+
+      <p style={{textAlign: 'center'}}><button onClick={reset}>Reset Photo</button></p>
+
+      <p style={{ color: '#f00', fontStyle: 'italic', border: '1px solid', textAlign: 'center', padding: '2px 0', borderRadius: '3px'}}>We need to check if export services support this CSS!</p>
+
 
     </section>
   );
